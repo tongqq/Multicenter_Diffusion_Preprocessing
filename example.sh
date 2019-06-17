586c7f0c400301f038449ef82e9e20b3d4be48de
tongqq <50980636+tongqq@users.noreply.github.com>
2019-06-17 23:07:31 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -8,20 +8,15 @@
 #                                                                      #
 ########################################################################
 
-diffname_AP=SMS_AP_S3P2_1_5ISO_00??
-diffname_PA=SMS_PA_S3P2_1_5ISO_00??
-acqtime=0.05796		#((146+24)/2-1)*0.69/1000		 (Number of Echos - 1) * Echo Spacing
-
-for subj in 1 2 3;do
-{
-for inst in LG XY MZ TJ TT ZN ZZ CY HN ZJU ZJU2 ZJU3;do #
-
+inst=ZJU
+subj=1
 DICOMDIR=/hongshan-share/program_brainatlas/data/Multicenter/${inst}TEST00${subj}_${inst}TEST00${subj}
 PREPRODIR=/hongshan-share/tongqiqi/BrainAtlas/ScientificData/Proc/${inst}_${subj}
 DATADIR=/hongshan-share/tongqiqi/BrainAtlas/ScientificData/UPLOAD_DATA/${inst}_${subj}
 
+diffname_AP=SMS_AP_S3P2_1_5ISO_00??
+diffname_PA=SMS_PA_S3P2_1_5ISO_00??
+acqtime=0.05796		#((146+24)/2-1)*0.69/1000		 (Number of Echos - 1) * Echo Spacing
+
 bash prep_diffusion.sh ${DICOMDIR} ${PREPRODIR} ${DATADIR} ${diffname_AP} ${diffname_PA} ${acqtime}
 
-done
-}&
-done
