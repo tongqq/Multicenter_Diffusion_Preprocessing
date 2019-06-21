9654b83b84125e71f491ef89f341c731ba981a3b
tongqq <50980636+tongqq@users.noreply.github.com>
2019-06-21 18:16:47 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -8,15 +8,15 @@
 #                                                                      #
 ########################################################################
 
-inst=ZJU
-subj=1
-DICOMDIR=/hongshan-share/program_brainatlas/data/Multicenter/${inst}TEST00${subj}_${inst}TEST00${subj}
-PREPRODIR=/hongshan-share/tongqiqi/BrainAtlas/ScientificData/Proc/${inst}_${subj}
-DATADIR=/hongshan-share/tongqiqi/BrainAtlas/ScientificData/UPLOAD_DATA/${inst}_${subj}
+inst = ZJU
+subj = 1
+DICOMDIR = $PWD/Dcm/${inst}TEST00${subj}_${inst}TEST00${subj}
+PREPRODIR = $PWD/Proc/${inst}_${subj}
+DATADIR = $PWD/DATA/${inst}_${subj}
 
-diffname_AP=SMS_AP_S3P2_1_5ISO_00??
-diffname_PA=SMS_PA_S3P2_1_5ISO_00??
-acqtime=0.05796		#((146+24)/2-1)*0.69/1000		 (Number of Echos - 1) * Echo Spacing
+diffname_AP = SMS_AP_S3P2_1_5ISO_00??
+diffname_PA = SMS_PA_S3P2_1_5ISO_00??
+acqtime = 0.05796		#((146+24)/2-1)*0.69/1000		 (Number of Echos - 1) * Echo Spacing [us]
 
 bash prep_diffusion.sh ${DICOMDIR} ${PREPRODIR} ${DATADIR} ${diffname_AP} ${diffname_PA} ${acqtime}
 
