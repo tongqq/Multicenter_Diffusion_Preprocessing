5a215a21c57f175db8c36aeb515b13572ab10c04
tongqq <50980636+tongqq@users.noreply.github.com>
2019-07-10 16:05:56 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -14,9 +14,12 @@ DICOMDIR = $PWD/Dcm/${inst}TEST00${subj}_${inst}TEST00${subj}
 PREPDIR = $PWD/Prep/${inst}_${subj}
 DATADIR = $PWD/DATA/${inst}_${subj}
 
+dcmname_T1=MP2RAGE_WIP900*_UNI-DEN_00??
 diffname_AP = SMS_AP_S3P2_1_5ISO_00??
 diffname_PA = SMS_PA_S3P2_1_5ISO_00??
 acqtime = 0.05796		#((146+24)/2-1)*0.69/1000		 (Number of Echos - 1) * Echo Spacing [us]
 
+
+bash prep_t1.sh ${DICOMDIR} ${PREPDIR} ${DATADIR} ${dcmname_T1}
 bash prep_diffusion.sh ${DICOMDIR} ${PREPDIR} ${DATADIR} ${diffname_AP} ${diffname_PA} ${acqtime}
 
