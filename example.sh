0cc874aff3979f893f8e91e0af3ce6737244f0af
tongqq <50980636+tongqq@users.noreply.github.com>
2020-01-05 15:13:17 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -18,10 +18,10 @@ DATADIR = $PWD/DATA/${inst}_${subj}
 dcmname_T1=MP2RAGE_WIP900*_UNI-DEN_00??
 diffname_AP = SMS_AP_S3P2_1_5ISO_00??
 diffname_PA = SMS_PA_S3P2_1_5ISO_00??
-acqtime = 0.050025		#0.345*(146-1)/1000=0.050025		  Echo Spacing * (Number of Echos - 1)  [us]
+acqtime = 0.050025		#0.345*(146-1)/1000=0.050025		  Echo Spacing * (Number of Echos - 1)  [sec]
 
 
 
-bash prep_diffusion.sh ${PROJDIR} ${DICOMDIR} ${PREPDIR} ${DATADIR} ${diffname_AP} ${diffname_PA} ${acqtime}
-bash prep_face_removal.sh ${PROJDIR} ${DICOMDIR} ${PREPDIR} ${DATADIR} ${dcmname_T1}
+bash Prep_diffusion.sh ${PROJDIR} ${DICOMDIR} ${PREPDIR} ${DATADIR} ${diffname_AP} ${diffname_PA} ${acqtime}
+bash Prep_face_removal.sh ${PROJDIR} ${DICOMDIR} ${PREPDIR} ${DATADIR} ${dcmname_T1}
 
