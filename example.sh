b075e6717bd5314ecebf25b8348b689b54971821
tongqq <50980636+tongqq@users.noreply.github.com>
2020-01-05 15:13:48 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -21,7 +21,5 @@ diffname_PA = SMS_PA_S3P2_1_5ISO_00??
 acqtime = 0.050025		#0.345*(146-1)/1000=0.050025		  Echo Spacing * (Number of Echos - 1)  [sec]
 
 
-
 bash Prep_diffusion.sh ${PROJDIR} ${DICOMDIR} ${PREPDIR} ${DATADIR} ${diffname_AP} ${diffname_PA} ${acqtime}
 bash Prep_face_removal.sh ${PROJDIR} ${DICOMDIR} ${PREPDIR} ${DATADIR} ${dcmname_T1}
-
